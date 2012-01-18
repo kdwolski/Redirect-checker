b71a3651906e3d5fe7a78cc0cbb557d599dc89f6
Kevin D. Wolski <kdwolski@gmail.com>
2012-01-17 20:54:22 -0500

    Committing changes to original source

--- follow.php
+++ follow.php
@@ -1,6 +1,13 @@
 <?php
-if (isset($_POST['url'])){
-    $url = urldecode($_POST['url']);
+if (isset($_POST['urls'])){
+   
+	//grab all the urls and place them into an array
+	$text = trim($_POST['urls']);
+	$textAr = explode("\n", $text);
+	$textAr = array_filter($textAr, 'trim'); // remove any extra \r characters left behind
+	
+	foreach ($textAr as $line) {
+	 $url = urldecode($line);
     //Validate the URL
         if ( validateUrl($url) ){
             //Append http to the url if it's not already there
@@ -9,31 +16,47 @@ if (isset($_POST['url'])){
                     
             //Fetch the redirects list
                 $redirects = redirects($url);
-
+			
             //Prepare the list of redirects for display
                 $output = "<ul class='redirects'>";
-
+				$output .= '<p class="tested-url">Checking: ' . $url . "</p>";
                 //Append each redirect to the list
                     $i = 1; //Used to count redirects
+                    $icon_class= ""; //make 404s stand out :)
                     foreach ( $redirects as $redirect ){
+                    	
+						if( strpos($redirect['response'], '404') > 0 ||  strpos($redirect['response'], '500') > 0){
+							$row_color="red";
+							$icon_class= "skull";
+						}else if(strpos($redirect['response'], '301') > 0){
+							$row_color="gray";
+							
+						}else if(strpos($redirect['response'], '200') > 0){
+							$row_color="green";
+							$icon_class= "finish-line";
+						}else{
+							$row_color="gray";
+						}
+						
                         $output .=
-                            "<li>" .
-                                "<h2>Step " . $i . "</h2>" .
-                                "<div class='location'>" . $redirect['location'] . "<a href='" . $redirect['location'] . "' rel='nofollow' target='_blank' title='Visit this link'><img class='externallink' src='external-link.gif'/></a></div>" .
-                                "<div class='response'>" . $redirect['response'] . "</div>" .
-                                "<div class='floatfix'></div>" .
-                            "</li>";
+                            '<li class="' .$row_color . '">' .
+                                '<h2>Step ' . $i . '</h2>' .
+                                '<div class="location">' . $redirect['location'] . '<a href="' . $redirect['location'] . '" rel="nofollow" target="_blank" title="Visit this link"><img class="externallink" src="external-link.gif"/></a></div>' .
+                                '<div class="response ' . $icon_class . '">' . $redirect['response'] . '</div>' .
+                                '<div class="floatfix"></div>' .
+                            '</li>';
                         $i++;
                     }
 
-                $output .= "</ul>";
-                $output .= "<p class='subtle'>This is the path web browsers and search engine crawlers follow to get to the page. The response code tells search engines where the content went. Beware of URL's that lead to nowhere and make sure permanently redirected content uses the 301 Moved Permanently response code so that search engines know which URL to index.</p>";
+                $output .= '</ul>';
+               // $output .= "<p class='subtle'>This is the path web browsers and search engine crawlers follow to get to the page. The response code tells search engines where the content went. Beware of URL's that lead to nowhere and make sure permanently redirected content uses the 301 Moved Permanently response code so that search engines know which URL to index.</p>";
         }
         else{
-            $output = "Please enter a valid URL";
+            $output = '<p class="invalid-url"><span>' . $url . '</span> is not a valid URL!</p>';
         }
 
         echo $output;
+}
 }
 //==============================================================================
     //Used to validate URLs
--- index.php
+++ index.php
@@ -13,6 +13,14 @@
                     event.preventDefault();
                     getredirects();
                 });
+
+				//clear initial textarea message
+				$('#urls').focus(function(){
+					if( $(this).val() == 'Enter URLs here, one per line.' ){
+						$(this).empty(); 
+					}
+				});
+                
             });
             function getredirects(){
                 $("#results").empty().append("<img class='loading' src='loading.gif'/>");
@@ -25,15 +33,17 @@
     </head>
     <body>
         <div id="wrapper">
-            <img src="header.png" alt="The page redirect checker"/>
+            <!-- <img src="header.png" alt="The page redirect checker"/> -->
+            <h1>Multiple URL Redirect Tester</h1>
             <form id="urlform" method="POST" action="">
-                <input type="text" name="url"/>
+                <textarea name="urls" id="urls" rows="20" cols="110">Enter URLs here, one per line.</textarea>
+                <p><input id="submit" name="submit" type="submit" value="Submit" /></p>
             </form>
             <div id="results">
                 <h2>What this tool does</h2>
                 <p>This tool will help you see what redirect leads
-                to the final page. Enter your URL in the page above and you will
-                see how it gets redirected to the final destination.The response codes will help you make sure that your website
+                to the final page. Enter your URLs in the form above (one URL per line) and you will
+                see how each gets redirected to the final destination. The response codes will help you make sure that your website
                 is using SEO-friendly 301 redirects.</p>
                 <h2>SEO-friendly redirects</h2>
                 <p>If you want to move content around, use 301 redirects to tell the search engines where it was moved. The 301
@@ -47,6 +57,9 @@
             </div>
             <div class="floatfix"></div>
         </div>
-        <div class="footer">Made by <a href="http://nicolasbouliane.com/" title="Nicolas Bouliane">Nicolas Bouliane</a></div>
+        <div class="footer">
+        	<p>Core source code by <a href="http://nicolasbouliane.com/" title="Nicolas Bouliane">Nicolas Bouliane</a></p>
+        	<p>@kdwolski - Modified to support batch URL checking and adjusted style of result display. Skull and Flag Icons by <a href="http://glyphish.com/" title="GLIPHISH Icons">GLIPHISH</a></p>
+        </div>
     </body>
 </html>
